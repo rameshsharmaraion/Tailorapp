2ecc675572893405c516046aec6224e4a9f05f9c
rameshsharmaraion <rameshsharmaraion@gmail.com>
2026-05-04 13:08:17 +0530

    Initial setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { ClientProvider } from './context/ClientContext.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ClientProvider>
         <App />
       </ClientProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
